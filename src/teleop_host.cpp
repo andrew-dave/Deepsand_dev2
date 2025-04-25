f7d542a6274af25cd77b9d12d9ffeca5c322d5b3
andrew-dave <evadaven.andrew@gmail.com>
2025-04-25 17:50:05 -0400

    host: publish servo angle along with cmd_vel

--- src/teleop_host.cpp
+++ src/teleop_host.cpp
@@ -1,12 +1,14 @@
 #include <rclcpp/rclcpp.hpp>
 #include <geometry_msgs/msg/twist.hpp>
+#include <std_msgs/msg/int16.hpp>
 #include <SDL2/SDL.h>
 #include <chrono>
 
 class TeleopNode : public rclcpp::Node {
 public:
     TeleopNode() : Node("teleop_host") {
-        pub_ = create_publisher<geometry_msgs::msg::Twist>("/cmd_vel", 10);
+        cmd_vel_pub_ = create_publisher<geometry_msgs::msg::Twist>("/cmd_vel", 10);
+        servo_pub_ = create_publisher<std_msgs::msg::Int16>("/blade_angle", 10);
         timer_ = create_wall_timer(std::chrono::milliseconds(100), std::bind(&TeleopNode::update, this));
         
         SDL_Init(SDL_INIT_VIDEO);
@@ -20,7 +22,8 @@ public:
 
 private:
     void update() {
-        geometry_msgs::msg::Twist msg;
+        geometry_msgs::msg::Twist cmd_vel_msg;
+        std_msgs::msg::Int16 servo_msg;
         SDL_Event event;
         while (SDL_PollEvent(&event)) {
             if (event.type == SDL_QUIT) {
@@ -29,23 +32,37 @@ private:
         }
 
         const Uint8* keys = SDL_GetKeyboardState(NULL);
+        // Robot teleoperation (WASD)
         if (keys[SDL_SCANCODE_W]) {
-            msg.linear.x = 1.0;  // Forward
+            cmd_vel_msg.linear.x = 1.0;  // Forward
         } else if (keys[SDL_SCANCODE_S]) {
-            msg.linear.x = -1.0; // Backward
+            cmd_vel_msg.linear.x = -1.0; // Backward
         }
         if (keys[SDL_SCANCODE_A]) {
-            msg.angular.z = 1.0; // Left
+            cmd_vel_msg.angular.z = 1.0; // Left
         } else if (keys[SDL_SCANCODE_D]) {
-            msg.angular.z = -1.0; // Right
+            cmd_vel_msg.angular.z = -1.0; // Right
         }
 
-        pub_->publish(msg);
+        // Servo control (Up/Down)
+        if (keys[SDL_SCANCODE_UP]) {
+            current_angle_ = std::min(current_angle_ + 10, 150);
+            servo_msg.data = current_angle_;
+            servo_pub_->publish(servo_msg);
+        } else if (keys[SDL_SCANCODE_DOWN]) {
+            current_angle_ = std::max(current_angle_ - 10, 50);
+            servo_msg.data = current_angle_;
+            servo_pub_->publish(servo_msg);
+        }
+
+        cmd_vel_pub_->publish(cmd_vel_msg);
     }
 
-    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr pub_;
+    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_pub_;
+    rclcpp::Publisher<std_msgs::msg::Int16>::SharedPtr servo_pub_;
     rclcpp::TimerBase::SharedPtr timer_;
     SDL_Window* window_;
+    int current_angle_ = 50; // Initial servo angle
 };
 
 int main(int argc, char *argv[]) {
